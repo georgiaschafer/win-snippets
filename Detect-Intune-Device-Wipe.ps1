733c523b2b0d51f5e0a84d7c600b6f6d710bfeb8
georgiaschafer <georgiaschafer@gmail.com>
2024-06-05 22:33:59 -0500

    Create Detect-Intune-Device-Wipe.ps1
    Custom Intune Device Wipe - Remediation script option, detection portion

---
+++ Detect-Intune-Device-Wipe.ps1
@@ -0,0 +1,37 @@
+#=============================================================================================================================
+#
+# Script Name:     Detect-Intune-Device-Wipe.ps1
+# Description:     Check for existance of scheduled task to direct Copilot requests to cdp.copilot.com and nochat.bing.com
+# Notes:           https://learn.microsoft.com/en-us/mem/intune/fundamentals/powershell-scripts-remediation
+#				   https://call4cloud.nl/2022/04/mamma-mia-here-we-wipe-again/
+#				   https://call4cloud.nl/wp-content/uploads/2022/03/wipeitall.zip
+# Author: 		   Georgia Schafer
+# Date:			   20240605
+# WARNING - SCRIPT WILL FORMAT SYSTEM IT IS RUN ON WHEN PAIRED WITH REMEDIATION SCRIPT - USE EXTREME CAUTION
+#
+#=============================================================================================================================
+
+# Define Variables
+$results = ""
+
+try
+{
+    $results = Get-ScheduledTask 'Intune-Data-Wipe' -ErrorAction Ignore
+    if (($results -ne $null)){
+        #Scheduled task found
+        Write-Host "Task-Found"
+        Return $results
+        exit 0
+    }
+    else{
+        #No matching scheduled task
+		#Below necessary for Intune as of 10/2019 will only remediate Exit Code 1
+        Write-Host "No-Task"        
+        exit 1
+    }   
+}
+catch{
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+}
